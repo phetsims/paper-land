ed57edbc03000cffd34303be0c190c149da2f176
Jesse <jesse.greenberg@colorado.edu>
2023-08-14 18:14:48 -0400

    form state is restored for editable enumeration controller components

--- client/creator/react/controller/CreateEnumerationControllerForm.js
+++ client/creator/react/controller/CreateEnumerationControllerForm.js
@@ -1,36 +1,41 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Form from 'react-bootstrap/Form';
 import styles from '../../CreatorMain.css';
 import EnumerationPropertyController from '../../model/controllers/EnumerationPropertyController.js';
+import useEditableForm from '../useEditableForm.js';
 
 export default function CreateEnumerationControllerForm( props ) {
 
-  // Reference to the current value (not state because we don't need a re-render)
-  const controlTypeValue = useRef( true );
+  const [ formData, handleChange ] = useEditableForm(
+    props.activeEdit,
+    props.isFormValid,
+    componentData => {
+      return !!componentData.controlType;
+    },
+    props.getFormData,
+    EnumerationPropertyController
+  );
 
   const controlTypes = EnumerationPropertyController.ControlType.enumeration.values;
-
-  const handleChange = event => {
-    controlTypeValue.current = event.target.value;
-
-    props.isFormValid( true );
-    props.getFormData( { controlType: controlTypeValue.current } );
-  };
-
   return (
     <div className={styles.controlElement}>
       <Form.Label>Choose how the paper events control the value:</Form.Label>
       {controlTypes.map( value => {
         const nameString = value === EnumerationPropertyController.ControlType.MARKERS ? 'Markers' :
                            'Rotation';
+
+        const valueString = value.toString();
         return <Form.Check
           name={'enumerationPropertyController'}
-          value={value}
+          value={valueString}
+          checked={valueString === formData.controlType}
           type={'radio'}
           id={`enum-${value}`}
           key={`enum-${value}`}
           label={nameString}
-          onChange={handleChange}
+          onChange={() => {
+            handleChange( { controlType: valueString } );
+          }}
         />;
       } )}
     </div>
