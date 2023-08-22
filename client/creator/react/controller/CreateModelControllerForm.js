c750364d6d992c8fb070c833521d455fa7f63639
Jesse <jesse.greenberg@colorado.edu>
2023-08-22 11:01:07 -0400

    update active controller tab with edit

--- client/creator/react/controller/CreateModelControllerForm.js
+++ client/creator/react/controller/CreateModelControllerForm.js
@@ -1,10 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import ListenerComponent from '../../model/controllers/ListenerComponent.js';
+import MultilinkListenerComponent from '../../model/controllers/MultilinkListenerComponent.js';
+import PropertyController from '../../model/controllers/PropertyController.js';
 import styles from './../../CreatorMain.css';
 import AnimationControllerForm from './AnimationControllerForm.js';
 import MultilinkControllerForm from './MultilinkControllerForm.js';
 import PaperControllerForm from './PaperControllerForm.js';
+
+/**
+ * Gets the tab value key for the active edit.
+ */
+const getTabForActiveEdit = activeEdit => {
+  if ( activeEdit && activeEdit.component instanceof PropertyController || activeEdit.component instanceof ListenerComponent ) {
+    const component = activeEdit.component;
+
+    if ( component instanceof PropertyController ) {
+      return 'paper';
+    }
+    else if ( component instanceof MultilinkListenerComponent ) {
+      return 'link';
+    }
+    else if ( component instanceof ListenerComponent ) {
+      return 'animation';
+    }
+    else {
+      throw new Error( 'Unknown controller type for tabs' );
+    }
+  }
+  else {
+    return null;
+  }
+};
 
 export default function CreateModelControllerForm( props ) {
 
@@ -23,6 +51,11 @@ export default function CreateModelControllerForm( props ) {
   if ( !props.model ) {
     throw new Error( 'CreateModelControllerForm requires a model' );
   }
+
+  // Set the selected tab when the active edit changes
+  useEffect( () => {
+    setSelectedTab( getTabForActiveEdit( activeEdit ) );
+  }, [ activeEdit ] );
 
   return (
     <>
