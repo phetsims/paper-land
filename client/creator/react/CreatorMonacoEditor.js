c36df65a540a81943b1ec381b01ba3d2d0ecf172
Jesse <jesse.greenberg@colorado.edu>
2023-10-18 13:48:22 -0400

    The CreatorMonacoEditor and code generator report syntax errors now, see https://github.com/phetsims/paper-land/issues/179

--- client/creator/react/CreatorMonacoEditor.js
+++ client/creator/react/CreatorMonacoEditor.js
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react';
+import { parse } from 'acorn';
+import React, { useRef, useState } from 'react';
 import MonacoEditor from 'react-monaco-editor';
 import styles from './../CreatorMain.css';
 
@@ -11,6 +12,9 @@ export default function CreatorMonacoEditor( props ) {
 
   const handleChange = props.handleChange || ( () => {} );
 
+  const [ hasError, setHasError ] = useState( false );
+  const [ errorMessage, setErrorMessage ] = useState( '' );
+
   // Ref to the current value of code (shouldn't trigger re-render)
   const codeString = useRef( '' );
 
@@ -19,27 +23,58 @@ export default function CreatorMonacoEditor( props ) {
     codeString.current = newValue;
   };
 
+  // Create a debounced version of the handleCodeChange function
+  const debouncedHandleCodeChange = useRef(
+    _.debounce( ( newValue, event ) => {
+      codeString.current = newValue;
+
+      // Use acorn to report when there is a syntax error in the current code
+      try {
+        parse( newValue, { ecmaVersion: 'latest' } );
+        setHasError( false );
+      }
+      catch( error ) {
+        setHasError( true );
+
+        if ( error.message ) {
+          setErrorMessage( error.message );
+        }
+      }
+    }, 2000 )
+  ).current;
+
   return (
-    <div className={`${styles.editor} ${styles.controlElement}`}>
-      <MonacoEditor
-        value={props.controlFunctionString}
-        language='javascript'
-        theme='vs-dark'
-        onChange={( newValue, event ) => {
+    <div>
+      <div className={`${styles.editor} ${styles.controlElement}`}>
+        <MonacoEditor
+          value={props.controlFunctionString}
+          language='javascript'
+          theme='vs-dark'
+          onChange={( newValue, event ) => {
 
-          // an empty string in case of undefined
-          const codeValue = newValue || '';
+            // an empty string in case of undefined
+            const codeValue = newValue || '';
 
-          handleCodeChange( codeValue, event );
-          handleChange( codeValue );
-        }}
-        options={{
-          tabSize: 2,
-          fontSize: '16px',
-          minimap: { enabled: false },
-          automaticLayout: true
-        }}
-      />
+            handleCodeChange( codeValue, event );
+
+            // Call the debounced function instead of handleCodeChange
+            debouncedHandleCodeChange( codeValue, event );
+
+            handleChange( codeValue );
+          }}
+          options={{
+            tabSize: 2,
+            fontSize: '16px',
+            minimap: { enabled: false },
+            automaticLayout: true
+          }}
+        />
+      </div>
+      {hasError && (
+        <div className={styles.validation}>
+          <h4>{`⚠ Syntax Error: Your code has a syntax error. ${errorMessage}`}</h4>
+        </div>
+      )}
     </div>
   );
 }
