a100d49446904e22371251e8eff28f70e03540cf
Jesse <jesse.greenberg@colorado.edu>
2023-11-06 14:02:35 -0500

    Improve the UX for editing templates by adding a warning dialog, see https://github.com/phetsims/paper-land/issues/161

---
+++ client/creator/react/ConfirmationDialog.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import styles from './../CreatorMain.css';
+import StyledButton from './StyledButton.js';
+
+export default function ConfirmationDialog( props ) {
+  if ( !props.onConfirm ) {
+    throw new Error( 'ConfirmationDialog requires an onConfirm callback.' );
+  }
+  if ( !props.confirmationContent ) {
+    throw new Error( 'ConfirmationDialog requires JSX for the confirmationContent' );
+  }
+
+  // Additional work to do when the action is cancelled, if any.
+  const onCancel = props.onCancel || ( () => {} );
+
+  // Customizable content for the dialog.
+  const confirmString = props.confirmString || 'Confirm';
+  const titleString = props.titleString || 'Are you sure?';
+
+  // State for whether the dialog is shown.
+  const [ show, setShow ] = useState( false );
+
+  const handleCancel = () => {
+    onCancel();
+    setShow( false );
+  };
+  const handleConfirm = () => {
+    props.onConfirm();
+    setShow( false );
+  };
+
+  // Show the dialog when props.showing is true
+  useEffect( () => {
+    setShow( props.showing );
+  }, [ props.showing ] );
+
+  return (
+    <>
+      <Modal show={show} onHide={handleCancel}>
+        <Modal.Header className={styles.dialog}>
+          <Modal.Title>{titleString}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className={styles.dialog}>{props.confirmationContent}</Modal.Body>
+        <Modal.Footer className={styles.dialog}>
+          <StyledButton variant='secondary' onClick={handleCancel} name={'Cancel'}></StyledButton>
+          <StyledButton variant='primary' onClick={handleConfirm} name={confirmString}></StyledButton>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
--- client/creator/react/SaveConfirmationDialog.js
+++ client/creator/react/SaveConfirmationDialog.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import styles from './../CreatorMain.css';
+import ConfirmationDialog from './ConfirmationDialog.js';
 import StyledButton from './StyledButton.js';
 
 export default function SaveConfirmationDialog( props ) {
@@ -9,10 +10,10 @@ export default function SaveConfirmationDialog( props ) {
   }
   const creatorModel = props.creatorModel;
 
-  // State for whether the dialog is shown.
+  // State for whether the dialog is shown - custom state for this component because it is controlled
+  // by the CreatorModel.
   const [ show, setShow ] = useState( false );
 
-  const handleShow = () => setShow( true );
   const handleCancel = () => {
     setShow( false );
   };
@@ -37,22 +38,18 @@ export default function SaveConfirmationDialog( props ) {
   const activeSpaceName = creatorModel.spaceNameProperty.value;
 
   return (
-    <>
-      <Modal show={show} onHide={handleCancel}>
-        <Modal.Header className={styles.dialog}>
-          <Modal.Title>Send to playground?</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className={styles.dialog}>{
-          <div>
-            <p>{`Warning! This will delete all existing programs in ${activeSpaceName}.`}</p>
-            <p>This project will also be saved.</p>
-          </div>
-        }</Modal.Body>
-        <Modal.Footer className={styles.dialog}>
-          <StyledButton variant='secondary' onClick={handleCancel} name={'Cancel'}></StyledButton>
-          <StyledButton variant='primary' onClick={handleSave} name={'Send to Playground'}></StyledButton>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <ConfirmationDialog
+      showing={show}
+      onConfirm={handleSave}
+      onCancel={handleCancel}
+      titleString={'Send to playground?'}
+      confirmationContent={
+        <div>
+          <p>{`⚠ Warning! This will delete all existing programs in ${activeSpaceName}.`}</p>
+          <p>This project will also be saved.</p>
+        </div>
+      }
+      confirmString={'Send to Playground'}
+    ></ConfirmationDialog>
   );
 }
