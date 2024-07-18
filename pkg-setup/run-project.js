0fb2bae25bfe5a836544ffe80a5898c7a520c685
Jesse <jesse.greenberg@colorado.edu>
2024-07-18 19:17:44 -0400

    trying out command-exists to require Node

--- pkg-setup/run-project.js
+++ pkg-setup/run-project.js
@@ -1,36 +1,48 @@
 const { execSync } = require( 'child_process' );
 const path = require( 'path' )
+const commandExists = require( 'command-exists' );
 
 // resolve the actual script path even when compiled with pkg (important for MacOS)
 const scriptDir = path.dirname( process.execPath );
 
-console.log( 'Curent working dir', process.cwd() );
-console.log( 'Executable path', process.execPath );
-console.log( 'Script dir', scriptDir );
+commandExists( 'node', ( err, commandExists ) => {
+  if ( err ) {
+    console.error( 'An error occurred while checking for Node.js.' );
+  }
+  else if ( !commandExists ) {
+    console.error( 'Node.js is not installed. Please install Node.js from https://nodejs.org/' );
+  }
+  else {
 
-// // Install dependencies
-try {
-  console.log( 'Installing dependencies. This may take a few minutes the first time...' );
+    console.log( 'Curent working dir', process.cwd() );
+    console.log( 'Executable path', process.execPath );
+    console.log( 'Script dir', scriptDir );
 
-  execSync( 'npm install', {
-    cwd: scriptDir,
-    stdio: 'inherit'
-  } );
-}
-catch( error ) {
-  console.error( 'Failed to install dependencies:', error );
-  process.exit( 1 );
-}
+    // Install dependencies
+    try {
+      console.log( 'Installing dependencies. This may take a few minutes the first time...' );
 
-// Start the project
-try {
-  console.log( 'Starting the project...' );
-  execSync( 'node server/entry-server.js', {
-    cwd: scriptDir,
-    stdio: 'inherit'
-  } );
-}
-catch( error ) {
-  console.error( 'Failed to start the project:', error );
-  process.exit( 1 );
-}
+      execSync( 'npm install', {
+        cwd: scriptDir,
+        stdio: 'inherit'
+      } );
+    }
+    catch( error ) {
+      console.error( 'Failed to install dependencies:', error );
+      process.exit( 1 );
+    }
+
+    // Start the project
+    try {
+      console.log( 'Starting the project...' );
+      execSync( 'node server/entry-server.js', {
+        cwd: scriptDir,
+        stdio: 'inherit'
+      } );
+    }
+    catch( error ) {
+      console.error( 'Failed to start the project:', error );
+      process.exit( 1 );
+    }
+  }
+} );
